c2f1da9e79e1e7492525bd087c863c34d21e07c7
Kimtk-00 <80250965+Kimtk-00@users.noreply.github.com>
2021-09-18 21:01:31 +0900

    Add files via upload

---
+++ split_data.py
@@ -0,0 +1,5 @@
+import splitfolders
+
+splitfolders.ratio("C:/Users/KTK/Desktop/swoon/0908",
+                   output="C:/Users/KTK/Desktop/swoon/0916",
+                   seed=1235, ratio=(.6, 0.2, 0.2), group_prefix=2)
